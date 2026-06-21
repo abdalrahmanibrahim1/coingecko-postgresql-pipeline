c7f20773730853e63d35d5e097c80e46a0c63fc6
abdal <aibrahim@adstratech.com>
2026-06-21 20:41:34 +0300

    Add CoinGecko API extraction

---
+++ src/extract.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def fetch_crypto_data():
+    url = (
+        "https://api.coingecko.com/api/v3/simple/price"
+        "?ids=bitcoin,ethereum"
+        "&vs_currencies=usd"
+        "&include_market_cap=true"
+        "&include_24hr_vol=true"
+        "&include_last_updated_at=true"
+    )
+
+    response = requests.get(url)
+    #response.raise_for_status()
+
+    return response.json()
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from extract import fetch_crypto_data
+
+
+data = fetch_crypto_data()
+print(data)
